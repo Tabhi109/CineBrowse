599a867019bb94cc411facaa6868bbb74d2a0a3f
Tabhi109 <tabhinav109@gmail.com>
2023-05-20 11:49:29 +0530

    ONDB API Added !

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import './App.css';
+import React, {useEffect} from "react";
+const API_URL = 'http://www.omdbapi.com?apikey=488f1a41';
+
 
 function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+
+  }
+
+  useEffect(() => {
+    searchMovies('spiderman');
+    
+  },[]);
   return (
     <div className="App">
       
